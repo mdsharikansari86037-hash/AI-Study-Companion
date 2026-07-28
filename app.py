6c942f3ee89c2cd77a78037c3fc7f951010643a2
mdsharikansari86037-hash <md.sharikansari86037@gmail.com>
2026-07-28 17:03:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if API_KEY:
 else:
     print("Warning: API Key not found in environment!")
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-3.6-flash')
 
 current_pdf_context = ""
 
